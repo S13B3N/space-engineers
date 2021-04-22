18bda14626f2bfb356d5fea579f6490b3fadedcb
S13B3N <sieben-@gmx.de>
2021-04-22 18:03:42 +0200

    added items

--- Autobuild/StorageManager.cs
+++ Autobuild/StorageManager.cs
@@ -17,20 +17,27 @@ namespace space_engineers.Autobuild.StorageManager
 
       // The amount of the items for the destination container
       Item[] itemList = {
-         new Display        (  5 ),
-         new Computer       ( 15 ),
-         new Construction   ( 25 ),
-         new Interiorplate  (  6 ),
-         new Explosives     ( 11 ),
-         new Girder         (  7 ),
-         new LargeSteelTube (  8 ),
-         new Motor          (  9 ),
-         new MetalGrid      ( 10 ),
-         new PowerCell      ( 11 ),
-         new SmallSteelTube ( 12 ),
-         new Steelplate     ( 13 ),
-         new SolarCell      ( 14 ),
-         new UraniumIngot   ( 18 ),
+         new BulletproofGlass ( 22 ),
+         new Canvas           ( 22 ),
+         new Computer         ( 15 ),
+         new Construction     ( 25 ),
+         new Display          (  5 ),
+         new Interiorplate    (  6 ),
+         new Explosives       ( 11 ),
+         new Girder           (  7 ),
+         new LargeSteelTube   (  8 ),
+         new Motor            (  9 ),
+         new MetalGrid        ( 10 ),
+         new Missile200       (  8 ),
+         new Nato5p56_45      (  8 ),
+         new Nato25_184       (  8 ),
+         new PowerCell        ( 11 ),
+         new SmallSteelTube   ( 12 ),
+         new Steelplate       ( 13 ),
+         new SolarCell        ( 14 ),
+         new SuperConductor   ( 12 ),
+         new Thruster         ( 12 ),
+         new UraniumIngot     ( 18 ),
       };
 
       //------------------------------------------------------------------------
@@ -41,7 +48,7 @@ namespace space_engineers.Autobuild.StorageManager
 
       public Program ()
       {
-         Runtime.UpdateFrequency = UpdateFrequency.Update10;
+         Runtime.UpdateFrequency = UpdateFrequency.Update100;
 
          ToCargoContainerList = new List<IMyTerminalBlock> ();
 
@@ -128,19 +135,26 @@ namespace space_engineers.Autobuild.StorageManager
 
       class Item { public int Amount { get; set; } public MyItemType Type { get; set; } public Item ( int amount ) { Amount = amount; }}
 
-      class Display        : Item { public Display        ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "Display"         ); }}
-      class Computer       : Item { public Computer       ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "Computer"        ); }}
-      class Construction   : Item { public Construction   ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "Construction"    ); }}
-      class Explosives     : Item { public Explosives     ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "Explosives"      ); }}
-      class Interiorplate  : Item { public Interiorplate  ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "InteriorPlate"   ); }}
-      class Girder         : Item { public Girder         ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "Girder"          ); }}
-      class LargeSteelTube : Item { public LargeSteelTube ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "LargeTube"       ); }}
-      class Motor          : Item { public Motor          ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "Motor"           ); }}
-      class MetalGrid      : Item { public MetalGrid      ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "MetalGrid"       ); }}
-      class PowerCell      : Item { public PowerCell      ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "PowerCell"       ); }}
-      class SmallSteelTube : Item { public SmallSteelTube ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "SmallTube"       ); }}
-      class Steelplate     : Item { public Steelplate     ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "SteelPlate"      ); }}
-      class SolarCell      : Item { public SolarCell      ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component", "SolarCell"       ); }}
-      class UraniumIngot   : Item { public UraniumIngot   ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Ingot"    , "Uranium"         ); }}
+      class BulletproofGlass : Item { public BulletproofGlass ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "BulletproofGlass" ); }}
+      class Canvas           : Item { public Canvas           ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "Canvas"           ); }}
+      class Computer         : Item { public Computer         ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "Computer"         ); }}
+      class Construction     : Item { public Construction     ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "Construction"     ); }}
+      class Display          : Item { public Display          ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "Display"          ); }}
+      class Explosives       : Item { public Explosives       ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "Explosives"       ); }}
+      class Interiorplate    : Item { public Interiorplate    ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "InteriorPlate"    ); }}
+      class Girder           : Item { public Girder           ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "Girder"           ); }}
+      class LargeSteelTube   : Item { public LargeSteelTube   ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "LargeTube"        ); }}
+      class Motor            : Item { public Motor            ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "Motor"            ); }}
+      class MetalGrid        : Item { public MetalGrid        ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "MetalGrid"        ); }}
+      class Missile200       : Item { public Missile200       ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_AmmoMagazine", "Missile200mm"     ); }}
+      class Nato5p56_45      : Item { public Nato5p56_45      ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_AmmoMagazine", "NATO_5p56x45mm"   ); }}
+      class Nato25_184       : Item { public Nato25_184       ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_AmmoMagazine", "NATO_25x184mm"    ); }}
+      class PowerCell        : Item { public PowerCell        ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "PowerCell"        ); }}
+      class SmallSteelTube   : Item { public SmallSteelTube   ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "SmallTube"        ); }}
+      class Steelplate       : Item { public Steelplate       ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "SteelPlate"       ); }}
+      class SolarCell        : Item { public SolarCell        ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "SolarCell"        ); }}
+      class SuperConductor   : Item { public SuperConductor   ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "Superconductor"   ); }}
+      class Thruster         : Item { public Thruster         ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Component"   , "Thrust"           ); }}
+      class UraniumIngot     : Item { public UraniumIngot     ( int amount ) : base ( amount ) { Type = new MyItemType ( "MyObjectBuilder_Ingot"       , "Uranium"          ); }}
    }
 }
